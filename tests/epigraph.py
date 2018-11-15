6037d63a5a7f2608af1e3f86872328c48f7e4fed
Thomas Vogt <thomas.vogt@uni-luebeck.de>
2018-11-15 13:40:10 +0100

    test for epigraph proj.

---
+++ tests/epigraph.py
@@ -0,0 +1,56 @@
+
+import numpy as np
+import cvxopt
+
+from opymize.operators.proj import EpigraphProj
+from opymize.tools.tests import test_gpu_op
+
+tol = 1e-10
+cvxopt.solvers.options['reltol'] = 1e-2*tol
+cvxopt.solvers.options['abstol'] = 1e-2*tol
+
+npoints = 3
+nfuns = 2
+nregions = 1
+nsubpoints = npoints
+I = np.array([[ True, False, False],
+              [False,  True,  True]], dtype=bool)
+J = np.arange(npoints)[None,:]
+b = np.zeros((nfuns,npoints))
+v = np.zeros((npoints, 2))
+x_in = np.zeros((nregions, nfuns, 3))
+x_out = x_in.copy()
+x = x_in.copy()
+
+for i in range(10):
+    v = -2.0 + 4*np.random.rand(*v.shape)
+    b = -2.0 + 4*np.random.rand(*b.shape)
+    op = EpigraphProj(I, J, v, b)
+
+    # x_in[0,0] : point on the plane (boundary of half space)
+    x_in[0,0,:] = -5.0 + 10*np.random.rand(3)
+    x_in[0,0,-1] = -b[0,0] + v[0].dot(x_in[0,0,:2])
+
+    # x_in[0,1] : point on the boundary
+    x_in[0,1,:] = -5.0 + 10*np.random.rand(3)
+    vals = [-bk + vk.dot(x_in[0,1,:2]) for bk,vk in zip(b[1,1:],v[1:])]
+    active_k = np.argmax(vals)
+    x_in[0,1,-1] = vals[active_k]
+
+    # x : point inside half space (mapped to itself)
+    x = x_in.copy()
+    x[0,:,-1] += 4*np.random.rand(2)
+    op(x, x_out)
+    assert np.linalg.norm(x_out - x) < tol
+
+    # x : point outside half space (mapped back to boundary)
+    x = x_in.copy()
+    shift = np.random.rand(2)
+    x[0,0,:2] += shift[0]*v[0]
+    x[0,0,-1] -= shift[0]
+    x[0,1,:2] += shift[1]*v[1+active_k]
+    x[0,1,-1] -= shift[1]
+    op(x, x_out)
+    assert np.linalg.norm(x_out - x_in) < tol
+
+    test_gpu_op(op)

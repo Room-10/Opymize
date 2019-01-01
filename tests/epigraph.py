9e6cc74814720c4332ae25d933a4927c0782dd4c
Thomas Vogt <admin@tovotu.de>
2019-01-02 00:42:08 +0100

    Tests for epigraph support function

--- tests/epigraph.py
+++ tests/epigraph.py
@@ -1,31 +1,33 @@
 
 import numpy as np
-import cvxopt
+np.set_printoptions(precision=4, linewidth=200, suppress=True, threshold=10000)
 
-from opymize.operators.proj import EpigraphProj
+from opymize.operators import EpigraphProj
+from opymize.functionals import EpigraphSupportFct
 from opymize.tools.tests import test_gpu_op
 
 tol = 1e-10
-cvxopt.solvers.options['reltol'] = 1e-2*tol
-cvxopt.solvers.options['abstol'] = 1e-2*tol
 
-ndim = 2
-npoints = 3
-nfuns = 2
-nregions = 1
-nsubpoints = npoints
-I = np.array([[ True, False, False],
-              [False,  True,  True]], dtype=bool)
-J = np.arange(npoints)[None,:]
-b = np.zeros((nfuns,npoints))
-v = np.zeros((npoints, ndim))
-x_in = np.zeros((nregions, nfuns, ndim+1))
-x_out = x_in.copy()
-x = x_in.copy()
+def test_proj_2d():
+    ndim = 2
+    npoints = 3
+    nfuns = 2
+    nregions = 1
+    nsubpoints = npoints
 
-for i in range(10):
-    v = -2.0 + 4*np.random.rand(*v.shape)
-    b = -2.0 + 4*np.random.rand(*b.shape)
+    I = np.zeros((nfuns, npoints), dtype=bool)
+    J = np.zeros((nregions, nsubpoints), dtype=np.int64)
+    I[:] = [[ True, False, False],
+            [False,  True,  True]]
+    J[:] = np.arange(npoints)[None,:]
+
+    v = -2.0 + 4*np.random.rand(npoints, ndim)
+    b = -2.0 + 4*np.random.rand(nfuns, npoints)
+
+    x_in = np.zeros((nregions, nfuns, ndim+1))
+    x_out = x_in.copy()
+    x = x_in.copy()
+
     op = EpigraphProj(I, J, v, b)
     op.cp_tol = tol*1e-3
 
@@ -56,3 +58,52 @@ for i in range(10):
     assert np.linalg.norm(x_out - x_in)**2 < tol
 
     test_gpu_op(op)
+
+def test_support_1d(nsubpoints):
+    ndim = 1
+    nfuns = 5
+    nregions = 2
+    npoints = nregions*nsubpoints
+
+    fct = lambda x, _a, _b, _c: _a*(x - _b)**2 + _c
+    x0 = 5*(-1 + 2*np.random.rand(nregions))
+    x1 = x0 + 0.1 + 2*np.random.rand(nregions)
+    a_arr = 0.1 + 4*np.random.rand(nfuns)
+    b_arr = x0[0] + (-1.5 + 4*np.random.rand(nfuns))*(x1[0] - x0[0])
+    c_arr = 5*(-1 + 2*np.random.rand(nfuns))
+
+    I = np.ones((nfuns, npoints), dtype=bool)
+    J = np.zeros((nregions, nsubpoints), dtype=np.int64)
+    J[:] = np.arange(npoints).reshape(J.shape)
+
+    v = np.zeros((nregions, nsubpoints, ndim), dtype=np.float64)
+    for j in range(nregions):
+        v[j,:] = np.linspace(x0[j], x1[j], nsubpoints)[:,None]
+        J[j,[1,-1]] = J[j,[-1,1]]
+    v = v.reshape(npoints, ndim)
+
+    b = np.zeros((nfuns, npoints), dtype=np.float64)
+    for i in range(nfuns):
+        b[i,:] = fct(v, a_arr[i], b_arr[i], c_arr[i]).ravel()
+
+    sigma = EpigraphSupportFct(I, J, v, b)
+    x = np.zeros((nregions, nfuns, ndim+1))
+    for j in range(nregions):
+        x[j,:,-1] = -np.random.rand()
+        x[j,:,0] = -x[j,:,-1]*(x0[j] + np.random.rand(nfuns)*(x1[j] - x0[j]))
+    f = np.zeros((nfuns, nregions))
+    for i in range(nfuns):
+        for j in range(nregions):
+            xx, xt = x[j,i,:-1], x[j,i,-1]
+            f[i,j] = -xt*fct(-xx/xt, a_arr[i], b_arr[i], c_arr[i])[0]
+    sx = sigma(x)
+    print("%.2e (%.2e)" % (np.abs(f.sum() - sx[0]), sx[1]))
+
+def main():
+    for N in [5, 50, 500, 5000]:
+        test_support_1d(N)
+    for i in range(10):
+        test_proj_2d()
+
+if __name__ == "__main__":
+    main()
